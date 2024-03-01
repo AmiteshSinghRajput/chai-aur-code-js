0cba77135a20a4030ca5c97d0744eb42556322eb
AmiteshSinghRajput <93336685+AmiteshSinghRajput@users.noreply.github.com>
2024-03-01 06:31:25 +0000

    operations update

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = undefined;
 
-console.log(typeof score);// always check type because when you reqeust body from frontend that time we don't know what data type it is
-console.log(typeof(score));
+// console.log(typeof score);// always check type because when you reqeust body from frontend that time we don't know what data type it is
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -16,7 +16,7 @@ console.log(valueInNumber);
 
 let isLoggedIn=1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=> true 0=> false
 //""=>false 
@@ -25,6 +25,43 @@ console.log(booleanIsLoggedIn);
 let someNumber=33
 
 let StringNumber = String(someNumber)
-console.log(StringNumber);
+// console.log(StringNumber);
+
+// ****************************************** OPERATIONS ************************************ 
+
+let value =3
+
+let negValue = - value
+
+// console.log(negValue);
+
+let str1= "namaste"
+let str2= " duniya"
+let str3= str1 + str2 
+console.log(str3);
+
+console.log("1"+2 +2);
+console.log(1+2 +"2");
+console.log(1+2 +"2"+(3+1));
+console.log(2 +2+"1"+"1");
+console.log(2 +2+"1");
+console.log("1"+(2+2));
+
+console.log(true);
+console.log(+true);
+
+
+let num1, num2, num3
+
+num1= num2= num3 = 2+2
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
 
 
