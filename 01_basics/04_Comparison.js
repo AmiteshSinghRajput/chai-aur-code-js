4211fb5d29cc5eecb5fb55d7c43b6d309a0e3cfd
AmiteshSinghRajput <93336685+AmiteshSinghRajput@users.noreply.github.com>
2024-03-01 07:00:28 +0000

    comparisons operator

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+// == and comparisons work differently comparisons convert null to 0
+
+console.log(undefined==NaN);
+console.log(undefined>=NaN);
+
+
+// === strict check
+
+console.log("2"===2); // think to learn more about strict check
